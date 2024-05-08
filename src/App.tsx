48d43f7af5b4ad595408e4c5baaefe274de276ee
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-05-08 14:56:41 +0100

    refactor: Update App component to use HeroSection and Footer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import Footer from "./components/footer";
+import HeroSection from "./components/hero";
+
 function App() {
   return (
-    <>
-      <div className="text-6xl">Hello Mum!</div>
-    </>
+    <div className="mx-auto lg:max-w-[140rem]">
+      <div className="lg:min-h-svh flex flex-col isolate">
+        <HeroSection />
+        <Footer />
+      </div>
+    </div>
   );
 }
 
