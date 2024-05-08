3533da802046fa5de56c3383dc9d3a654e4c41bf
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-05-08 07:37:35 +0100

    feat: Add new colors and font family to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark_gray: "hsl(0, 0%, 63%)",
+        very_dark_gray: "hsl(0, 0%, 27%)",
+      },
+      fontFamily: {
+        league_spartan: ["League Spartan", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
