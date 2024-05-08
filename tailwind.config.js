3b34bc4ea51a6ba0d76e4b2241b3fdfb6b15f3ec
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-05-08 14:55:34 +0100

    feat: Add responsive screen sizes to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "640px",
+
+      md: "768px",
+
+      lg: "1280px",
+    },
     extend: {
       colors: {
         dark_gray: "hsl(0, 0%, 63%)",
