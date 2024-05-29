00e3368d2bf52082d5e5f47fdc42ee1292ffa40f
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-05-29 12:46:20 +0100

    refactor: Add sticky header with Navbar component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import Footer from "./components/footer";
 import HeroSection from "./components/hero";
+import { Navbar } from "./components/navbar";
+import { pageData } from "./data/data";
 
 function App() {
   return (
-    <div className="mx-auto lg:max-w-[140rem]">
+    <div className="mx-auto lg:max-w-[140rem] relative">
+      <header className="sticky z-10">
+        <Navbar />
+      </header>
       <div className="lg:min-h-svh flex flex-col">
-        <HeroSection />
+        <HeroSection info={pageData} />
         <Footer />
       </div>
     </div>
